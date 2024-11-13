a8185936ae5445efb2e27bd1c815122be802c312
JABRI Raed <jabriraed80@gmail.com>
2024-11-13 19:48:25 +0100

    change title and logo in file index.html

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <div>
+                <div>Rouda</div>
+                <div>
+                        <a href="#home">Home</a>
+                        <a href="#home">About Me</a>
+                        <a href="#home">Services</a>
+                        <a href="#home">Projects</a>
+                        <a href="#home">Contact</a>
+                </div>
+                <button>Connect Me</button>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
