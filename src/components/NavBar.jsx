ad7a53951eec8686089858419943eaea9d1e68ac
JABRI Raed <jabriraed80@gmail.com>
2024-11-13 20:20:22 +0100

    create compnent NavBar // just i d'ont write link for page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-        <div>
-                <div>Rouda</div>
-                <div>
-                        <a href="#home">Home</a>
-                        <a href="#home">About Me</a>
-                        <a href="#home">Services</a>
-                        <a href="#home">Projects</a>
-                        <a href="#home">Contact</a>
+    <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
+        <div className='container py-2 flex justify-center md:justify-between items-center '>
+                <div className='text-2xl font-bold hidden md:inline '>Rouda</div>
+                <div className='space-x-6 '>
+                        <a href="#home" className='hover:text-gray-400 '>Home</a>
+                        <a href="#aboutMe" className='hover:text-gray-400 '>About Me</a>
+                        <a href="#services" className='hover:text-gray-400 '>Services</a>
+                        <a href="#projects" className='hover:text-gray-400 '>Projects</a>
+                        <a href="#contact" className='hover:text-gray-400 '>Contact</a>
                 </div>
-                <button>Connect Me</button>
+                <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
+                 transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+                  Connect Me
+                </button>
         </div>
-    </div>
+    </nav>
   )
 }
 
